4f5de26692a92d1302284ffbc820b739c9b386f7
GSGranados <stevengranados0211@gmail.com>
2020-07-26 13:33:06 -0600

    Navbar updated
    Drawer removed from App Component and some links were added to the Navbar

--- src/App.js
+++ src/App.js
@@ -7,19 +7,11 @@ import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
 import Button from "@material-ui/core/Button";
-//Drawer imports
-import clsx from "clsx";
-import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
-import List from "@material-ui/core/List";
-import Divider from "@material-ui/core/Divider";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
 //Content Management
 import { Grid } from "@material-ui/core";
 import Router from "./components/Router/Router";
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -32,9 +24,11 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
     fontFamily: "'Bebas Neue', cursive",
+
   },
   option: {
     fontFamily: "'Bebas Neue', cursive",
+    color: "white",
   },
   list: {
     width: 250,
@@ -46,80 +40,30 @@ const useStyles = makeStyles((theme) => ({
 
 function App() {
   const classes = useStyles();
-  const [state, setState] = React.useState({
-    left: false,
-  });
-
-  const toggleDrawer = (anchor, open) => (event) => {
-    if (
-      event &&
-      event.type === "keydown" &&
-      (event.key === "Tab" || event.key === "Shift")
-    ) {
-      return;
-    }
-
-    setState({ ...state, [anchor]: open });
-  };
-
-  const list = (anchor) => (
-    <div
-      className={clsx(classes.list, {
-        [classes.fullList]: anchor === "top" || anchor === "bottom",
-      })}
-      role="presentation"
-      onClick={toggleDrawer(anchor, false)}
-      onKeyDown={toggleDrawer(anchor, false)}
-    >
-      <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
-    </div>
-  );
 
   return (
     <div className={classes.root}>
       {/*AppBar component call*/}
       <AppBar position="static" style={{ backgroundColor: "black" }}>
         <Toolbar variant="dense">
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-            onClick={toggleDrawer("left", true)}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" color="inherit" className={classes.title}>
-            Marvel App
+            <Link to="/" style={{ textDecoration: "none" , color: "white" }}>
+              Marvel App
+            </Link>
           </Typography>
-          <Button className={classes.option} color="inherit">Login</Button>
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://github.com/GSGranados"
+            style={{ textDecoration: "none" }}
+          >
+            <Button className={classes.option} color="inherit">
+              GSGranados
+            </Button>
+          </a>
         </Toolbar>
       </AppBar>
       {/*AppBar component call*/}
-      {/* Drawer Call*/}
-      <SwipeableDrawer
-        anchor={"left"}
-        open={state["left"]}
-        onClose={toggleDrawer("left", false)}
-        onOpen={toggleDrawer("left", true)}
-      >
-        {list("left")}
-      </SwipeableDrawer>
-      {/* Drawer Call*/}
       {/* Content with routes */}
       <Grid
         container
@@ -129,7 +73,7 @@ function App() {
         style={{ minHeight: "100vh" }}
         className="app-content"
       >
-          <Router></Router>
+        <Router></Router>
       </Grid>
     </div>
   );
